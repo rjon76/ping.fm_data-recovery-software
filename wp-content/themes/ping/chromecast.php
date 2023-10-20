21e44922d0eb80d32a128b96ba32212e329de18e
ihor <zamozhnii@gmail.com>
2023-10-20 15:58:48 +0300

    fix

--- wp-content/themes/ping/chromecast.php
+++ wp-content/themes/ping/chromecast.php
@@ -28,7 +28,7 @@
 							</div>
 						</div>
 					</div>
-					<p class="updated"><?php if ( get_the_modified_time() != get_the_time()) pll_e('Last update on '); ?><time datetime="<?php echo the_modified_time( 'c' ); ?>"><?php echo the_modified_date('F d, Y'); ?></time></p>
+					<p class="updated"><?php if ( get_the_modified_time() != get_the_time()) pll_e('Last update on '); ?> <time datetime="<?php echo the_modified_time( 'c' ); ?>"><?php echo ' '. the_modified_date('F d, Y'); ?></time></p>
 				</div>
 				<?php
 					the_content();
