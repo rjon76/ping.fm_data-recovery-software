741d024fdc1c6943b14ac2ee938f5257e89a16be
ihor <zamozhnii@gmail.com>
2023-11-19 16:34:05 +0200

    fix

--- wp-content/themes/ping/form-page.php
+++ wp-content/themes/ping/form-page.php
@@ -358,26 +358,26 @@ error_reporting(E_ALL);
                     });
                 }
             });
+        });
 
-            function openTab(evt, tabName) {
-                let i, tabcontent, tablinks;
+        function openTab(evt, tabName) {
+            let i, tabcontent, tablinks;
 
-                tabcontent = document.getElementsByClassName("tabcontent");
-                for (i = 0; i < tabcontent.length; i++) {
-                    tabcontent[i].style.display = "none";
-                }
-
-                tablinks = document.getElementsByClassName("tablinks");
-                for (i = 0; i < tablinks.length; i++) {
-                    tablinks[i].className = tablinks[i].className.replace(" active", "");
-                }
-
-                document.getElementById(tabName).style.display = "block";
-                evt.currentTarget.className += " active";
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display = "none";
             }
 
-            document.getElementById("defaultOpen").click();
-        });
+            tablinks = document.getElementsByClassName("tablinks");
+            for (i = 0; i < tablinks.length; i++) {
+                tablinks[i].className = tablinks[i].className.replace(" active", "");
+            }
+
+            document.getElementById(tabName).style.display = "block";
+            evt.currentTarget.className += " active";
+        }
+
+        document.getElementById("defaultOpen").click();
         </script>
     </body>
 </html>
