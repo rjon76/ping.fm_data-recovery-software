f7cb6508d23d1d8b7447e2cdb81ad4ba25af00ce
ihor <zamozhnii@gmail.com>
2023-11-20 18:08:06 +0200

    fix

--- wp-content/themes/ping/form-page.php
+++ wp-content/themes/ping/form-page.php
@@ -28,6 +28,8 @@ error_reporting(E_ALL);
         $json = json_encode($xml);
         $arrayArticles = json_decode($json, TRUE);
 
+        var_dump($arrayArticles);
+
         if(count($arrayArticles) > 0) {
             if(count($arrayArticles) == 1) {
                 $arrayLastArticle = $arrayArticles["page"];
