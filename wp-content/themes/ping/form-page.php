c329fbe8d4c350abbced9f89c1e941998832d410
ihor <zamozhnii@gmail.com>
2023-11-24 20:43:15 +0200

    fix

--- wp-content/themes/ping/form-page.php
+++ wp-content/themes/ping/form-page.php
@@ -486,7 +486,7 @@ error_reporting(E_ALL);
                         </form>
                     </div>
                 <?php } else { ?>
-                    <div class="loader show">
+                    <div class="loader show" id="loaderModal" data-stepsf="<?php echo home_url() . '/wp-content/uploads/time_record.txt'; ?>">
                         <h1 class="stepTitle"><?php echo $infoStepText; ?></h1>
                         <svg version="1.1" id="L1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve">
                             <circle fill="none" stroke="#fff" stroke-width="6" stroke-miterlimit="15" stroke-dasharray="14.2472,14.2472" cx="50" cy="50" r="47" >
@@ -795,7 +795,7 @@ error_reporting(E_ALL);
 
             const startInterval = setInterval(() => {
                 jQuery.ajax({
-                    url: jQuery("#article").attr('data-stepsf'),
+                    url: jQuery("#loaderModal").attr('data-stepsf'),
                     dataType: 'text',
                     success: function (data) {
                         console.log(data)
